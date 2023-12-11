f82659078c16901079c179fc0e97a410dd09e17c
melvin <mmpriyemskiy@gmail.com>
2023-12-10 22:55:09 -0500

    finished 1 chart

--- src/DashboardPage/DashboardPage.js
+++ src/DashboardPage/DashboardPage.js
@@ -1,24 +1,13 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Chart } from 'chart.js';
 
 function DashboardPage() {
   const navigate = useNavigate();
-
-  function LandingPage() {
-    localStorage.removeItem('jwt');
-    navigate('/');
-  }
-
-  function BudgetPage() {
-    navigate('/budgetpage');
-  }
-
-  function ExpensePage() {
-    navigate('/expensepage');
-  }
-
+  const chartRef = useRef(null);
   const [budgetInfo, setBudgetInfo] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState('');
 
   // Fetch budget information on component mount
   useEffect(() => {
@@ -31,26 +20,42 @@ function DashboardPage() {
         }
       })
       .then(response => {
-          console.log(response)
-          //divide up the response
-          
+        // Divide up the response
+        var budgetArray = response.data.budget;
+        var catagoryArray = response.data.catagory;
+        var expenses = response.data.expense;
+
+        // Set budget info state
+        setBudgetInfo(budgetArray);
+
+        // Create or update the chart
+        createChart(chartRef.current, catagoryArray, budgetArray, expenses, selectedMonth);
       })
       .catch(error => {
         console.error('Error fetching budget information:', error);
       });
     }
-  }, []); // Empty dependency array to run only once on mount
+  }, [selectedMonth]); // Update the chart when the selected month changes
+
+  // Function to handle month selection
+  const handleMonthChange = (event) => {
+    setSelectedMonth(event.target.value);
+  };
+
+  // Create an array of all months
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
 
   return (
     <div>
-      DashboardPage
       <div>
-        <button onClick={LandingPage}>Logout</button>
-        <button onClick={BudgetPage}>Configure Budget</button>
-        <button onClick={ExpensePage}>Add Expenses</button>
+        <button onClick={() => navigate('/')}>Logout</button>
+        <button onClick={() => navigate('/budgetpage')}>Configure Budget</button>
+        <button onClick={() => navigate('/expensepage')}>Add Expenses</button>
       </div>
 
-      {/* Display budget information */}
       <div>
         <h2>Budget Information</h2>
         <ul>
@@ -59,8 +64,92 @@ function DashboardPage() {
           ))}
         </ul>
       </div>
+
+      {/* Dropdown for month selection */}
+      <label>Select Month:</label>
+      <select onChange={handleMonthChange} value={selectedMonth}>
+        <option value="">All Months</option>
+        {months.map((month, index) => (
+          <option key={index} value={month}>{month}</option>
+        ))}
+      </select>
+
+      {/* Chart canvas */}
+      <canvas id="budgetExpenseChart" ref={chartRef} width="800" height="400"></canvas>
     </div>
   );
 }
+
+function createChart(chartRef, categories, budget, expenses, selectedMonth) {
+  if (!chartRef) return;
+
+  const ctx = chartRef.getContext('2d');
+
+  // Check if a chart instance already exists
+  if (ctx.chart) {
+    // Destroy the existing chart instance
+    ctx.chart.destroy();
+  }
+
+  // Prepare data
+  const budgetLabels = categories;
+  const budgetData = budget;
+
+  // Filter expenses based on the selected month or show all months if none selected
+  const filteredExpenses = selectedMonth
+    ? expenses.filter(expense => expense.tag === selectedMonth)
+    : expenses;
+
+  console.log('Filtered Expenses:', filteredExpenses);
+
+  // Use accumulated expense data for the selected month(s)
+  const organizedExpenseData = [];
+  for (let i = 0; i < budgetLabels.length; i++) {
+    const category = budgetLabels[i];
+    const totalExpense = filteredExpenses
+      .filter(expense => expense.title === category)
+      .reduce((sum, expense) => sum + expense.budget, 0);
+
+    organizedExpenseData.push(totalExpense);
+  }
+
+  console.log('Organized Expense Data:', organizedExpenseData);
+
+  // Create the chart
+  const newChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: budgetLabels,
+      datasets: [
+        {
+          label: 'Budget',
+          data: budgetData,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1,
+        },
+        {
+          label: 'Expenses',
+          data: organizedExpenseData,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+
+  // Attach the new chart instance to the canvas element
+  ctx.chart = newChart;
+}
+
+
 
 export default DashboardPage;
